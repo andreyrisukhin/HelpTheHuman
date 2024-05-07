5e2cbc165969d743186f75dbb9a3595d61ab042e
Kavel Rao <kavelrao@cs.washington.edu>
2024-05-07 09:19:49 -0700

    red: replace lstm with linear

--- Color-Maze/run_ppo.py
+++ Color-Maze/run_ppo.py
@@ -57,8 +57,10 @@ class ActorCritic(nn.Module):
         self.feature_linear = nn.Sequential(
             layer_init(nn.Linear(64*6*6 + 3, 192)),
             nn.Tanh(),
+            layer_init(nn.Linear(192, 192)),
+            nn.Tanh(),
         ).to(device)
-        self.lstm = nn.LSTM(self.lstm_hidden_size, self.lstm_hidden_size, batch_first=True).to(device)
+        # self.lstm = nn.LSTM(self.lstm_hidden_size, self.lstm_hidden_size, batch_first=True).to(device)
         self.policy_network = nn.Sequential(
             layer_init(nn.Linear(self.lstm_hidden_size, 64)),
             nn.Tanh(),
@@ -88,19 +90,19 @@ class ActorCritic(nn.Module):
         features = torch.cat((features, goal_info), dim=1)
         features = self.feature_linear(features)
 
-        # Pass through LSTM; add singular sequence length dimension for LSTM input
-        features = features.reshape(batch_size, 1, -1)
-        if prev_hidden_and_cell_states is not None:
-            prev_hidden_states = prev_hidden_and_cell_states[0].reshape(1, batch_size, self.lstm_hidden_size)
-            prev_cell_states = prev_hidden_and_cell_states[1].reshape(1, batch_size, self.lstm_hidden_size)
-            prev_hidden_and_cell_states = (prev_hidden_states, prev_cell_states)
-        features, (hidden_states, cell_states) = self.lstm(features, prev_hidden_and_cell_states)
+        # # Pass through LSTM; add singular sequence length dimension for LSTM input
+        # features = features.reshape(batch_size, 1, -1)
+        # if prev_hidden_and_cell_states is not None:
+            # prev_hidden_states = prev_hidden_and_cell_states[0].reshape(1, batch_size, self.lstm_hidden_size)
+            # prev_cell_states = prev_hidden_and_cell_states[1].reshape(1, batch_size, self.lstm_hidden_size)
+            # prev_hidden_and_cell_states = (prev_hidden_states, prev_cell_states)
+        # features, (hidden_states, cell_states) = self.lstm(features, prev_hidden_and_cell_states)
 
         # Grab all batches and remove sequence length dimension
         # features: (batch_size, 1, feature_size)
         # last_timestep_features: (batch_size, feature_size)
         last_timestep_features = features[:, -1, ...].squeeze(1)
-        return self.policy_network(last_timestep_features), self.value_network(last_timestep_features), (hidden_states, cell_states)
+        return self.policy_network(last_timestep_features), self.value_network(last_timestep_features), (torch.zeros((batch_size, self.lstm_hidden_size), device=self.device), torch.zeros((batch_size, self.lstm_hidden_size), device=self.device))
 
     def get_value(self, x, goal_info, prev_hidden_and_cell_states: tuple | None = None):
         return self.forward(x, goal_info=goal_info, prev_hidden_and_cell_states=prev_hidden_and_cell_states)[1]
